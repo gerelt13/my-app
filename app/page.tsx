178ddede969a1d9fd2d20ca115d279684ac0a4eb
gerelt <geredorj@gmail.com>
2023-11-16 20:59:05 -0600

    message1

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { Content, Mulish } from "next/font/google";
-import React from "react";
 // import MiddleText from "../next/MiddleText";
+import Image from "next/image";
 import BackgroundImage from "../public/brooke.png";
-import { Input } from "postcss";
-import { connected } from "process";
 import { NavigationItem } from "./navigation-item";
-import { url } from "inspector";
 const EmailInputStyle = {
   fontSize: "18px",
   border: "1px solid black",
@@ -17,10 +13,6 @@ const EmailInputStyle = {
   fontFamily: "Mulish",
   marginTop: "40px",
 };
-
-import Image from "next/image";
-import { Component } from "react";
-import { Review } from "./review";
 
 const Texts = () => {
   return (
@@ -161,46 +153,56 @@ export default function Home() {
           <Button />
         </main>
       </div>
- 
 
-<div style={{  }}>
-  <h1
-    style={{
-      height: "120px",
-      fontWeight: "800px",
-      width: "398px",
-      fontSize: "48px",
-    }}
-  >
-    Your Hub for teamwork
-  </h1>
+      <div style={{}}>
+        <h1
+          style={{
+            height: "120px",
+            fontWeight: "800px",
+            width: "398px",
+            fontSize: "48px",
+          }}
+        >
+          Your Hub for teamwork
+        </h1>
 
-  <div style={{ display: "flex", flexDirection: "column" }}>
-    <p
-      style={{
-        height: "120px",
-        fontWeight: "800px",
-        width: "398px",
-        fontSize: "18px",
-        fontFamily: "Mulish",
-      }}
-    >
-      Give everyone you work with—inside and outside your company—a more
-      productive way to stay in sync. Respond faster with emoji, keep
-      conversations focused in channels, and simplify all your communication
-      into one place.
-    </p>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <p
+            style={{
+              height: "120px",
+              fontWeight: "800px",
+              width: "398px",
+              fontSize: "18px",
+              fontFamily: "Mulish",
+            }}
+          >
+            Give everyone you work with—inside and outside your company—a more
+            productive way to stay in sync. Respond faster with emoji, keep
+            conversations focused in channels, and simplify all your
+            communication into one place.
+          </p>
 
-    <a style={{ height: " 28px", fontSize: "16px", width: "87px" }} href="/">
-      Learn more <svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12.1188 3.00392H1.10876C0.558765 3.00392 0.108765 3.45392 0.108765 4.00392C0.108765 4.55392 0.558765 5.00392 1.10876 5.00392H12.1188V6.79392C12.1188 7.24392 12.6588 7.46392 12.9688 7.14392L15.7488 4.35392C15.9388 4.15392 15.9388 3.84392 15.7488 3.64392L12.9688 0.853923C12.6588 0.533923 12.1188 0.763923 12.1188 1.20392V3.00392Z" fill="#4DA0FD"/>
-</svg>
-
-
-    </a>
-    <img src="ootoMeetings.png"></img>
-  </div>
-</div>
-</div>
+          <a
+            style={{ height: " 28px", fontSize: "16px", width: "87px" }}
+            href="/"
+          >
+            Learn more
+            <svg
+              width="16"
+              height="8"
+              viewBox="0 0 16 8"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12.1188 3.00392H1.10876C0.558765 3.00392 0.108765 3.45392 0.108765 4.00392C0.108765 4.55392 0.558765 5.00392 1.10876 5.00392H12.1188V6.79392C12.1188 7.24392 12.6588 7.46392 12.9688 7.14392L15.7488 4.35392C15.9388 4.15392 15.9388 3.84392 15.7488 3.64392L12.9688 0.853923C12.6588 0.533923 12.1188 0.763923 12.1188 1.20392V3.00392Z"
+                fill="#4DA0FD"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <Image src="ootoMeetings.png" style={{ width: "1179px" }} />
+    </div>
   );
 }
